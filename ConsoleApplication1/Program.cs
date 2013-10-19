ed23db7a9fadcec1ade9848981f3eb51ce5538d3
Willstah <Willstah@Willstah-PC>
2013-10-18 21:01:05 -0400

    initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleReciever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s;
+            int recv;
+            byte[] data = new byte[1024];
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 1165);
+            Socket newsock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            newsock.Bind(ipep);
+            EndPoint Remote = (EndPoint)(ipep);
+            recv = newsock.ReceiveFrom(data, ref Remote);
+            while (true)
+            {
+                data = new byte[1024];
+                recv = newsock.ReceiveFrom(data, ref Remote);
+                s = Encoding.ASCII.GetString(data, 0, recv);
+                Console.WriteLine( s.Substring(34, 20));
+                newsock.SendTo(data, recv, SocketFlags.None, Remote);
+            }
+        }
+    }
+}
