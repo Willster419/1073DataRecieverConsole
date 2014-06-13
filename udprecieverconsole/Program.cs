1541de957becc03d7e2cfc3d5cb56e916ffcbb57
Willster419 <willster419@outlook.com>
2014-06-12 22:03:07 -0400

    cleaned it up and made the console use the UDPClient class instead of "making" a client

--- udprecieverconsole/Program.cs
+++ udprecieverconsole/Program.cs
@@ -11,33 +11,27 @@ namespace ConsoleReciever
         static void Main(string[] args)
         {
             String s;
-            int recv;
             byte[] data;
             IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 1165);
-            Socket newsock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            newsock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-            newsock.Bind(ipep);
-            EndPoint Remote = (EndPoint)(ipep);
+            UdpClient client = new UdpClient(ipep);
+            client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
             String c;
-            int crecv;
             byte[] cdata;
             IPEndPoint cipep = new IPEndPoint(IPAddress.Any, 1166);
-            Socket cnewsock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            cnewsock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-            cnewsock.Bind(cipep);
-            EndPoint cRemote = (EndPoint)(cipep);
+            UdpClient Cclient = new UdpClient(cipep);
+            Cclient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
             while (true)
           {
-                data = new byte[1024];
-                recv = newsock.ReceiveFrom(data, ref Remote);
-                s = Encoding.ASCII.GetString(data, 0, recv);
-                Console.WriteLine(s.Substring(35,75));//only what we need to see
-                cdata = new byte[1024];
-                crecv = cnewsock.ReceiveFrom(cdata, ref cRemote);
-                c = Encoding.ASCII.GetString(cdata, 0, crecv);
-
-                //Console.ReadLine(); //this will pause the console
-                //Console.WriteLine(c); //we dont want to do this quite yet
+              data = client.Receive(ref ipep);
+              s = Encoding.ASCII.GetString(data);
+              Console.WriteLine(s.Substring(35,75));//only what we need to see
+              
+              //don't need the console stuff right now...
+              //cdata = Cclient.Receive(ref cipep);
+              //c = Encoding.ASCII.GetString(cdata);
+              //Console.ReadLine(); //this will pause the console
+              //Console.WriteLine(c); //we dont want to do this quite yet
             }
         }
     }
